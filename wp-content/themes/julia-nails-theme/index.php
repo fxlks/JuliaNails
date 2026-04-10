243d0939722a08db6721f063c9d6cf82ca6cff50
fxlks <karmar01@easv365.dk>
2026-04-10 12:35:30 +0200

    adding core tempate part to index

--- wp-content/themes/julia-nails-theme/index.php
+++ wp-content/themes/julia-nails-theme/index.php
@@ -1,8 +1,11 @@
-<?php get_header() ?>
+<?php
+get_header(); // Loads header.php
 
+if ( have_posts() ) : 
+    while ( have_posts() ) : the_post(); 
+        the_content(); // This is what pulls in your Gutenberg/Kadence blocks
+    endwhile; 
+endif; 
 
-
-
-
-
-<?php get_footer() ?>
+get_footer(); // Loads footer.php
+?>

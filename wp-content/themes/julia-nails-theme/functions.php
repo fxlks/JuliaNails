e6ad147a75019d247a4a7409682ae2cdbeea123e
fxlks <karmar01@easv365.dk>
2026-04-17 09:27:13 +0200

    added font 1

--- wp-content/themes/julia-nails-theme/functions.php
+++ wp-content/themes/julia-nails-theme/functions.php
@@ -3,3 +3,13 @@ function jn_load_resources() {
     wp_enqueue_style('style', get_stylesheet_directory_uri() . '/assets/css/style.css');
 }
 add_action('wp_enqueue_scripts', 'jn_load_resources');
+
+function add_typekit_fonts() {
+    wp_enqueue_style(
+        'typekit-fonts', 
+        'https://use.typekit.net/wzy5fff.css',
+        array(), 
+        null
+    );
+}
+add_action('wp_enqueue_scripts', 'add_typekit_fonts');

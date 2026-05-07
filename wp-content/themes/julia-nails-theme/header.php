8715b2ac9c730ed9fa1f7dc797829e7f765e3e06
Ivankirev39 <ivakir01@easv365.dk>
2026-05-07 18:23:43 +0200

    Change alt text of the logo in the navbar

--- wp-content/themes/julia-nails-theme/header.php
+++ wp-content/themes/julia-nails-theme/header.php
@@ -12,7 +12,7 @@
     <div class="container navbar-flex">
         <div class="site-logo">
             <a href="<?php echo esc_url( home_url( 'Home' ) ); ?>" aria-label="Julia Nails - Go to homepage">
-                <img src="http://julianails.local/wp-content/uploads/2026/04/ChatGPT-Image-Apr-20-2026-09_56_21-PM.png" alt="Logo">
+                <img src="http://julianails.local/wp-content/uploads/2026/04/ChatGPT-Image-Apr-20-2026-09_56_21-PM.png" alt="Junails Studio Logo">
             </a>
         </div>
 
